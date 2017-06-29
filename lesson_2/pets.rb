8cabcb7a443c0da3a2e87f0ce041b6ef341fe16c
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-06-29 11:41:08 -0400

    Lesson 2 OOP exercises

---
+++ lesson_2/pets.rb
@@ -0,0 +1,30 @@
+class Pet
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+end
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+end
+
+class Cat < Pet
+  def speak
+    "Mrreow!"
+  end
+
+end
