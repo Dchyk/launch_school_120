a7ff72c94cfd339807e5bd9b3e671b96f4dda593
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-07-07 09:33:01 -0400

    OOP RPS bonus features

--- lesson_2/rps_bonus_2.rb
+++ lesson_2/rps_bonus_2.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
+class GameHistory
+  attr_accessor :history, :game
+
+  def initialize
+    @history = {}
+    @game = 1
+  end
+end
+
 class Move
   VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
@@ -26,7 +35,6 @@ class Move
   def spock?
     @value == 'spock'
   end
-
 
   def >(other_move)
     (rock? && (other_move.scissors? || other_move.lizard?)) ||
@@ -103,26 +111,28 @@ class Score
   end
 
   def add_win
-    @wins += 1
+    self.wins += 1
   end
 
-  def winning_score?
-    @wins >= 10
+  def ten_wins?
+    self.wins >= 10
   end
 end
 
 class RPSGame
-  attr_accessor :human, :computer, :human_wins, :comp_wins
+  attr_accessor :human, :computer, :human_score, :comp_score, :game_data,
+                :round_winner
 
   def initialize
-    @human = Human.new
-    @computer = Computer.new
-    @human_wins = Score.new
-    @comp_wins = Score.new
+    self.human = Human.new
+    self.computer = Computer.new
+    self.human_score = Score.new
+    self.comp_score = Score.new
+    self.game_data = GameHistory.new
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts "Welcome to Rock, Paper, Scissors! First to 10 wins overall!"
   end
 
   def display_goodbye_message
@@ -130,35 +140,42 @@ class RPSGame
   end
 
   def display_moves
+    puts ""
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
   end
 
-  def display_winner
+  def record_winner
     if human.move > computer.move
-      puts "#{human.name} won!"
-      human_wins.add_win
+      human_score.add_win
+      self.round_winner = human.name
     elsif human.move < computer.move
-      puts "#{computer.name} won!"
-      comp_wins.add_win
-    else
-      puts "It's a tie!"
+      comp_score.add_win
+      self.round_winner = computer.name
     end
   end
 
+  def display_winner
+    puts ""
+    puts "#{@round_winner} won!"
+  end
+
   def display_wins
-    puts "#{human.name} has #{human_wins.wins} wins."
-    puts "#{computer.name} has #{comp_wins.wins} wins."
+    puts ""
+    puts "#{human.name} has #{human_score.wins} wins."
+    puts "#{computer.name} has #{comp_score.wins} wins."
   end
 
   def display_overall_winner
-    puts "#{human.name} is the overall winner!" if human_wins.winning_score?
-    puts "#{computer.name} is the overall winner!" if comp_wins.winning_score?
+    puts ""
+    puts "#{human.name} is the overall winner!" if human_score.ten_wins?
+    puts "#{computer.name} is the overall winner!" if comp_score.ten_wins?
   end
 
   def play_again?
     answer = nil
     loop do
+      puts ""
       puts "Would you like to play again? (y/n)"
       answer = gets.chomp
       break if ['y', 'n'].include?(answer.downcase)
@@ -169,20 +186,59 @@ class RPSGame
     return true if answer.downcase == 'y'
   end
 
+  def store_game_history
+    game_data.history[game_data.game] = { computer: computer.move.to_s,
+                                          human: human.move.to_s, winner: round_winner.to_s }
+  end
+
+  def increment_game_number
+    game_data.game += 1
+  end
+
+  def display_game_history
+    puts ""
+    game_data.history.each do |game, data|
+      puts "Game #{game}. #{computer.name}: #{data[:computer]} " \
+                        " #{human.name}: #{data[:human]}" \
+                        " #{round_winner} won!"
+    end
+  end
+
+  def players_choose
+    human.choose
+    computer.choose
+  end
+
+  def record_game_data
+    record_winner
+    store_game_history
+  end
+
+  def display_game_results
+    display_moves
+    display_winner
+    display_wins
+  end
+
+  def player_has_ten_wins?
+    human_score.ten_wins? || comp_score.ten_wins?
+  end
+
   def play
     display_welcome_message
 
     loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
-      display_wins
-      if human_wins.winning_score? || comp_wins.winning_score?
+      system 'clear'
+      players_choose
+      record_game_data
+      display_game_results
+      display_game_history
+      if player_has_ten_wins?
         display_overall_winner
         break
       end
       break unless play_again?
+      increment_game_number
     end
     display_goodbye_message
   end
