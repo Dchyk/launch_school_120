831f648bce7e14a69713cf28589eec6db4f7e0bf
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-07-17 17:59:30 -0400

    OOP Twentyone

--- lesson_5/twentyone.rb
+++ lesson_5/twentyone.rb
@@ -1,69 +1,108 @@
-class Participant
-  # What goes here? All redundant behaviors from Player and Dealer?
-  attr_accessor :cards
+require 'pry'
 
-  def initialize(cards)
-    # What would a "data" or "states" of a Player object entail
-    # cards, name, score
-    @cards = cards
+module Hand
+  WINNING_HAND = 21
+
+  def >(other_player)
+    hand_total > other_player.hand_total
   end
 
-  def stay
+  def <(other_player)
+    hand_total < other_player.hand_total
+  end
+
+  # May not need this? 
+  def ==(other_player)
+    hand_total == other_player.hand_total
+  end
+
+  def hand_total
+    total = 0
+    self.cards.each do |card|
+      total += card.value
+      total -= 10 if total > WINNING_HAND && card.face == 'A' # adjust for Aces
+    end
+
+    total
   end
 
   def busted?
-    @cards > 21
+    hand_total > WINNING_HAND
+  end
+end
+
+class Participant
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
   end
 
-  def total
-    # seems like we'll need to know about "cards" to calculate this
+  def display_cards
+    cards = []
+    @cards.each do |card|
+      cards << "#{card.face} of #{card.suit}"
+    end
+    cards.join(', ')
   end
-
 end
 
 class Player < Participant
 
-  def hit
-    # Choose whether to hit or stay
-  end
+  include Hand
 
+  def hit(deck)
+    @cards << deck.deal_card
+  end
 end
 
-class Dealer
-  
-  def deal
-    # does the dealer deal, or does the deck deal?
-  end
+class Dealer < Participant
 
-  def hit
-    # hits up to 17
-  end
+  include Hand
 
-end
-
-
-class Deck
-  
-  def initialize
-    # Needs some data structure to hold the deck
-    # An array of card objects?
-    # Used an array in the procedural version (because suit doesn't matter?)
-  end
-
-  
-
-  def deal_first_hand
-    # Does dealer deal, or does deck deal?
-    2.times do
-      player.cards += deck.deal_a_card
-      dealer.cards += deck.deal_a_card
+  # Dealer will keep hitting until its hand is at least 17
+  def hit(deck)
+    loop do
+      break if hand_total >= 17
+      @cards << deck.deal_card
     end
   end
 end
 
+class Deck
+
+  def initialize
+    @deck = create_new_deck
+  end
+
+  def create_new_deck
+    deck = []
+    Card::SUITS.each do |suit|
+      Card::FACES.each do |face|
+        deck << Card.new(suit, face)
+      end
+    end
+    deck
+  end
+
+  def shuffle!
+    @deck.shuffle!
+  end
+
+  def deal_card
+    @deck.pop
+  end
+
+end
+
 class Card
+  CARD_VALUES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
+                 '8' => 8, '9' => 9, '10' => 10, 'J' => 10,  'Q' => 10, 
+                 'K' => 10, 'A' => 11 }
   SUITS = ['C', 'D', 'H', 'S']
   FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+  attr_reader :suit, :face
 
   def initialize(suit, face)
     # What are the "states" of a card?
@@ -72,37 +111,18 @@ class Card
   end
 
   def to_s
+    "#{@face} of #{@suit}"
   end
 
-
-end
-
-module Hand
-  def show
-    # display self.cards 
-  end
-
-  def cards
-    @cards
-  end
-
-  def >(other_hand)
-    cards.determine_total > other_hand.cards.determine_total
-  end
-
-  def <(other_hand)
-    cards.determine_total < other_hand.cards.determine_total
-  end
-
-  def determine_total
-    # Add up the cards in the hand
-    # Adjust for Aces
-    # Return the integer value of the total
+  def value
+    CARD_VALUES[face]
   end
 
 end
 
 class Game
+  attr_reader :deck, :dealer, :player
+
   def initialize
     @dealer = Dealer.new
     @player = Player.new
@@ -110,46 +130,75 @@ class Game
   end
 
   def start
+    display_welcome_message
+    shuffle_deck
     deal_cards
-    show_initial_cards
+    show_cards
     player_turn
-    dealer_turn
+    if player.busted?
+      busted_message
+    else
+      dealer_turn
+    end
+    show_cards
     show_result
   end
 
-  def deal_cards
-    deck.deal_first_hand
+  def shuffle_deck
+    deck.shuffle!
   end
 
-  def show_initial_cards
-    player.cards.show
-    dealer.cards.show
+  def deal_cards
+    2.times do 
+      dealer.cards << deck.deal_card
+      player.cards << deck.deal_card
+    end
+  end
+
+  def show_cards
+    puts "Dealer has: #{dealer.display_cards}. Total score: #{dealer.hand_total}"
+    puts "Player has: #{player.display_cards}. Total score: #{player.hand_total}"
+    puts ""
   end
 
   def player_turn
-    # loop 
-    # IF you want to hit, 
-    player.hit
-    # break if not or if 
-    player.busted?
+    input = nil
+    loop do
+      break if player.busted? 
+      puts "Do you want to hit or stay? Any key to hit, or type (s)tay:"
+      input = gets.chomp
+      break if %w(stay).include?(input) || player.busted?
+      player.hit(deck)
+      show_cards
+    end
   end
 
   def dealer_turn
-    dealer.hit # up to 17
-    # break if 
-    dealer.busted? || dealer.hand >= 17
+    dealer.hit(deck)
   end
 
   def show_result
-    puts "score:"
-    if player.hand > dealer.hand 
-      puts "Player won!"
-    elsif dealer.hand > player.hand
-      puts "Dealer won!"
-    else 
+    if dealer.busted?
+      puts "Dealer busted! You win!"
+    elsif !player.busted? && player > dealer
+      puts "Player wins!"
+    elsif dealer > player 
+      puts "Dealer wins!"
+    elsif player == dealer
       puts "It's a tie!"
     end
   end
+
+  def busted_message
+    puts "You busted! game over, dealer wins!"
+  end
+
+  def display_welcome_message
+    system 'clear'
+    puts "Hello and welcome to 21!"
+    puts ""
+  end
+
 end
 
 Game.new.start
